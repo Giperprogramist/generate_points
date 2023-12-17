72818fbf94c043cca738ba7287abf3a7cefe6ff0
Giperprogramist <144926858+Giperprogramist@users.noreply.github.com>
2023-12-17 22:31:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def generate_points(*args):
-    return ['.' * num for num in args]
+def generate_and_print_points(a, b, c):
+    input_numbers = (a, b, c)
+    points_list = ['.' * num for num in input_numbers]
+    for points in points_list:
+        print(points)
+    return points_list
 
-result = generate_points(2, 4, 3)
-print(result)
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+
+output_points = generate_and_print_points(a, b, c)
